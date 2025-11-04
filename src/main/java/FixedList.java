bff3c3b6cb5548f177d1b628670ed72fbf6f2de1
montasia harris <mharris@my.yearupunited.com>
2025-11-04 10:43:09 -0500

    Generics exercise WRKBKWK6

--- src/main/java/FixedList.java
+++ src/main/java/FixedList.java
@@ -1,8 +1,23 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class FixedList <T>{
-    private ArrayList<T> items;
-    public FixedList(){
-        items = new ArrayList<T>();
+    private List<T> items;
+    private int maxsize;
+
+    public FixedList(List<T> items, int maxsize) {
+        this.items = items;
+        this.maxsize = maxsize;
+
     }
+    private void add(T item){
+        if(items.size()<maxsize){
+            items.add(item);
+        }
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
 }
